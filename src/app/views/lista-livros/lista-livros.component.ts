f76c73ecaf239c558fb572a84cbf481e4139266b
Salomao <salomao.machado.75@gmail.com>
2023-06-27 23:11:37 -0300

    Refatoração: Criação de classe para melhor distribuir funções; todas as tarefas de  solicitação de dados estão alicadas na classe.

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,6 +1,7 @@
-import { VolumeInfo, ImageLinks } from './../../models/interfaces';
+import { VolumeInfo, ImageLinks, Item } from './../../models/interfaces';
 import { Component, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
+import { LivroVolumeInfo } from 'src/app/models/LivroVolumeInfo';
 import { Livro } from 'src/app/models/interfaces';
 import { LivroService } from 'src/app/service/livro.service';
 
@@ -25,21 +26,10 @@ export class ListaLivrosComponent implements OnDestroy{
     })
   }
 
-  livrosResultadoParaLivros(itens): Livro[]{
-    const livros: Livro[] = [];
-
-    itens.forEach(item => {
-      livros.push(this.livro = {
-        title: item.volumeInfo?.title,
-        authors: item.volumeInfo?.authors,
-        publisher: item.volumeInfo?.publisher,
-        publishedDate: item.volumeInfo?.publishedDate,
-        description: item.volumeInfo?.description,
-        previewLink: item.volumeInfo?.previewLink,
-        thumbnail: item.volumeInfo?.imageLinks?.thumbnail
-      })
+  livrosResultadoParaLivros(items: Item[]): LivroVolumeInfo[]{
+    return items.map(item => {
+      return new LivroVolumeInfo(item)
     })
-    return livros;
   }
 
   ngOnDestroy(){
