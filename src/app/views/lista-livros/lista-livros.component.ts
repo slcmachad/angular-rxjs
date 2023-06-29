2a8b2061a94a979137367a028bc10ba9cccafa33
Salomao <salomao.machado.75@gmail.com>
2023-06-29 16:37:39 -0300

    Catch e throw errors: utilização de metodos para capturar e informar erros na aplicação.

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,9 +1,11 @@
 import { FormControl } from '@angular/forms';
 import { Item } from './../../models/interfaces';
 import { Component } from '@angular/core';
-import { map, switchMap, tap } from 'rxjs';
+import { EMPTY, catchError, debounceTime, filter, map, switchMap, tap, throwError } from 'rxjs';
 import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
 import { LivroService } from 'src/app/service/livro.service';
+
+const pausa = 600;
 
 @Component({
   selector: 'app-lista-livros',
@@ -12,15 +14,24 @@ import { LivroService } from 'src/app/service/livro.service';
 })
 export class ListaLivrosComponent {
   campoBusca = new FormControl();
+  mensagemErro = '';
 
   constructor(private service : LivroService) { }
 
   livrosEncontrados$ = this.campoBusca.valueChanges
       .pipe(
+        debounceTime(pausa),
+        filter((valorDigitado) => valorDigitado.length > 2),
         tap(() => console.log('inicio')),
         switchMap((valorDigitado) => this.service.buscar(valorDigitado)),
-        tap(() => console.log('requisição')),
-        map((items) => this.livrosResultadoParaLivros(items))
+        tap((retornoAPI) => console.log(retornoAPI)),
+        map((items) => this.livrosResultadoParaLivros(items)),
+        catchError(() => {
+          this.mensagemErro = 'Obs, algo deu errado, recarregue a aplicação.'
+          return EMPTY
+/*           console.log(erro)
+          return throwError(() => new Error(this.mensagemErro = 'Obs, algo deu errado, recarregue a aplicação.')) */
+        })
       )
 
   livrosResultadoParaLivros(items: Item[]): LivroVolumeInfo[]{
